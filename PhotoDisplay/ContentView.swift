a13e02a0545916b9057acd484c1f72157ac10923
saibharadwaj135 <saibharadwaj135@gmail.com>
2025-01-25 21:08:32 -0500

    Initial Commit

---
+++ PhotoDisplay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoDisplay
+//
+//  Created by Sai bharadwaj Adapa on 1/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoDisplay/PhotoDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoDisplayApp.swift
+//  PhotoDisplay
+//
+//  Created by Sai bharadwaj Adapa on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
